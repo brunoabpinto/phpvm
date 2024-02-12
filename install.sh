2a7d293284f8757d0021daa650efb4311e18b8e0
Bruno Pinto <bruno.pinto@inboxagency.com>
2024-02-12 23:44:53 +0000

    installer prevent reinstall

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 if [ -f "$HOME/.zshrc" ]; then
-    echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.zshrc
-    echo 'PHPVM installed in zshell. Reloading shell'
-    cd -
-    zsh
+    if ! grep -q ".phpvm.sh" $HOME/.zshrc; then
+        echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.zshrc
+        echo 'PHPVM installed in zshell. Reloading shell'
+        zsh
+    else
+        echo 'PHPVM already installed in .zshrc. Skipping'
+    fi
 elif [ -f "$HOME/.bashrc" ]; then
-    echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.bashrc
-    echo 'PHPVM installed in bash. Reloading shell'
-    cd -
-    bash
+    if ! grep -q ".phpvm.sh" $HOME/.bash; then
+        echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.bashrc
+        echo 'PHPVM installed in bash. Reloading shell'
+        bash
+    else
+        echo 'PHPVM already installed in .zshrc. Skipping'
+    fi
 else
     cd -
     echo 'Please install zshell or bash to run PHPVM'
