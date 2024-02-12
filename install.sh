0dcb24f79f927a214589aa35304dcde211be718a
Bruno Pinto <bruno.pinto@inboxagency.com>
2024-02-12 23:22:20 +0000

    installer

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -f "$HOME/.zshrc" ]; then
-    echo  '\n\n#PHPVM source\nif [[ -r "$HOME/phpvm.sh" ]];then\n\tsource "$HOME/phpvm.sh"\nfi'  >>  $HOME/.zshrc
+    echo  '\n\n#PHPVM source\nif [[ -r "$HOME/phpvm/phpvm.sh" ]];then\n\tsource "$HOME/phpvm/phpvm.sh"\nfi'  >>  $HOME/.zshrc
     echo 'PHPVM installed in zshell. Reloading shell'
     zsh
 elif [ -f "$HOME/.bashrc" ]; then
-    echo  '\n\n#PHPVM source\nif [[ -r "$HOME/phpvm.sh" ]];then\n\tsource "$HOME/phpvm.sh"\nfi'  >>  $HOME/.bashrc
+    echo  '\n\n#PHPVM source\nif [[ -r "$HOME/phpvm/phpvm.sh" ]];then\n\tsource "$HOME/phpvm/phpvm.sh"\nfi'  >>  $HOME/.bashrc
     echo 'PHPVM installed in bash. Reloading shell'
     bash
 else
