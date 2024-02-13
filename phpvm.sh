3904a0283cfa0c45ace7b24aa4f7afa8c2db6f54
Bruno Pinto <bruno.pinto@inboxagency.com>
2024-02-13 00:24:04 +0000

    default mechanism

--- phpvm.sh
+++ phpvm.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+# PHPVM default PHP version
+if [[ -r "$HOME/phpvm/default.sh" ]]; then
+	source "$HOME/phpvm/default.sh"
+fi
 
 function phpvm()
     if [[ $1 == "install" ]]; then
@@ -16,17 +19,10 @@ function phpvm()
 
     elif [[ $1 == "default" ]]; then
         for VERSION in /opt/homebrew/opt/php@$2*; do
-            if ! grep -q ".phpvm/default.sh" $HOME/.zshrc; then
-                print '\n\n# PHPVM default PHP version\nif [[ -r "$HOME/.phpvm/default.sh" ]]; then\n\tsource "$HOME/.phpvm/default.sh"\nfi' >> $HOME/.zshrc
-            fi
-            if ! grep -q ".phpvm/default.sh" $HOME/.bashrc; then
-                print '\n\n# PHPVM default PHP version\nif [[ -r "$HOME/.phpvm/default.sh" ]]; then\n\tsource "$HOME/.phpvm/default.sh"\nfi' >> $HOME/.bashrc
-            fi
             print "export PATH=\"${VERSION}/bin:\$PATH\"" > $HOME/phpvm/default.sh
             zsh
             break
         done
-        php -v
 
     elif [[ $1 == "list" ]]; then
         for VERSION in /opt/homebrew/opt/php@$2*; do
