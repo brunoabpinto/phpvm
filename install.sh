e8c2b2368856a9f3667105460fa504b7cb996956
Bruno Pinto <bruno.pinto@inboxagency.com>
2024-02-13 00:11:51 +0000

    installer

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 if [ -f "$HOME/.zshrc" ]; then
     if ! grep -q ".phpvm.sh" $HOME/.zshrc; then
-        echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.zshrc
+        printf  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.zshrc
         echo 'PHPVM installed in zshell. Reloading shell'
-        zsh
     else
         echo 'PHPVM already installed in .zshrc. Skipping'
     fi
 fi
 if [ -f "$HOME/.bashrc" ]; then
-    if ! grep -q ".phpvm.sh" $HOME/.bash; then
-        echo  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.bashrc
-        echo 'PHPVM installed in bash. Reloading shell'
-        bash
+    if ! grep -q ".phpvm.sh" $HOME/.bashrc; then
+        printf  "\n\n#PHPVM source\nif [[ -r \"$HOME/phpvm/phpvm.sh\" ]];then\n\tsource \"$HOME/phpvm/phpvm.sh\"\nfi"  >>  $HOME/.bashrc
     else
         echo 'PHPVM already installed in .zshrc. Skipping'
     fi
 fi
-if [ ! -f "$HOME/.zshrc" &&  ! -f "$HOME/.bashrc"]; then
-    echo 'Please install zshell or bash to run PHPVM'
+
+# Reload shell to apply changes
+if [ $SHELL == '/bin/zsh' ]; then
+    zsh
+elif [ $SHELL == '/bin/bash' ]; then
+    bash
 fi
